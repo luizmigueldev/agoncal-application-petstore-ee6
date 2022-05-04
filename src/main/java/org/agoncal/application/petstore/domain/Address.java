91ec717af29b1aeaa23c6e3f5763a607d8ae737a
luizmigueldev <luizmigueln.dev@gmail.com>
2022-05-04 13:02:53 -0300

    exclusão das implementações manuais get - set

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -21,21 +23,21 @@ public class Address {
     @Column(nullable = false)
     @NotNull
     @Size(min = 5, max = 50)
-    private String street1;
-    private String street2;
+    @Getter @Setter private String street1;
+    @Getter @Setter private String street2;
     @Column(nullable = false, length = 100)
     @NotNull
     @Size(min = 2, max = 50)
-    private String city;
-    private String state;
+    @Getter @Setter private String city;
+    @Getter @Setter private String state;
     @Column(name = "zip_code", nullable = false, length = 10)
     @NotNull
     @Size(min = 1, max = 10)
-    private String zipcode;
+    @Getter @Setter private String zipcode;
     @Column(nullable = false, length = 50)
     @NotNull
     @Size(min = 2, max = 50)
-    private String country; // TODO use an enum
+    @Getter @Setter private String country; // TODO use an enum
 
     // ======================================
     // =            Constructors            =
@@ -48,58 +50,6 @@ public class Address {
         this.street1 = street1;
         this.city = city;
         this.zipcode = zipcode;
-        this.country = country;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public String getStreet1() {
-        return street1;
-    }
-
-    public void setStreet1(String street1) {
-        this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
-    }
-
-    public void setStreet2(String street2) {
-        this.street2 = street2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
         this.country = country;
     }
 
